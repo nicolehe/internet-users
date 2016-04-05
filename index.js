c0bd196c3c7cb64c853e7d2eb41fb1bdca0733f8
nicolehe <nicool@gmail.com>
2016-04-05 19:08:50 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,35 @@
+var countriesWithInternetPercent = {};
+
+_.each(gapminderData, function(datum) {
+    var countryName = datum['Internet users (per 100 people)'];
+    var internetPercent = datum['2011'];
+    var countryNumber;
+
+    //making a json object, making a key with "countryName" and assigning a value, "internetPercent"
+    countriesWithInternetPercent[countryName] = internetPercent;
+
+});
+
+console.log(countriesWithInternetPercent);
+
+    document.write('<svg class="chart" width="3000" height="5600" id="graph">');
+_.each(countriesWithInternetPercent, function(oneCountryWithInternetPercent, oneCountryName) {
+    var bar = window.innerWidth * (Math.ceil(oneCountryWithInternetPercent) /110);
+    var textLocation = bar + 5;
+    var index = Object.keys(countriesWithInternetPercent).indexOf(oneCountryName);
+    var x = index * 37;
+    var transform = index * 30;
+    var r = 150;
+    var g = Math.ceil(oneCountryWithInternetPercent/100 * 255);
+    var b = 120;
+
+    document.write('  <g transform = "translate(0,' + transform + ')">');
+    document.write('  <rect width="' + bar + '" height="29" style="fill:rgb(' + r +', ' + g +', ' + b + ')"></rect>');
+    document.write('  <text x="' + textLocation + '" y="15" dy=".35em">' + oneCountryName + ' (' + Math.ceil(oneCountryWithInternetPercent) + '%)' +'</text>');
+    document.write('  </g>');
+
+    
+
+});
+
+    document.write('</svg>');
---
+++ internet-per-100-2011.js
@@ -0,0 +1,750 @@
+var gapminderData = [
+  {
+    "2011": 96.6183615047022,
+    "Internet users (per 100 people)": "Iceland"
+  },
+  {
+    "2011": 93.4547589983845,
+    "Internet users (per 100 people)": "Norway"
+  },
+  {
+    "2011": 92.1272194417825,
+    "Internet users (per 100 people)": "Netherlands"
+  },
+  {
+    "2011": 90.8820455939913,
+    "Internet users (per 100 people)": "Sweden"
+  },
+  {
+    "2011": 90.7038249323017,
+    "Internet users (per 100 people)": "Luxembourg"
+  },
+  {
+    "2011": 89.9772981700753,
+    "Internet users (per 100 people)": "Denmark"
+  },
+  {
+    "2011": 89.3330044366066,
+    "Internet users (per 100 people)": "Finland"
+  },
+  {
+    "2011": 88.8507244513137,
+    "Internet users (per 100 people)": "Bermuda"
+  },
+  {
+    "2011": 86.2,
+    "Internet users (per 100 people)": "Qatar"
+  },
+  {
+    "2011": 86.1817338599836,
+    "Internet users (per 100 people)": "New Zealand"
+  },
+  {
+    "2011": 85,
+    "Internet users (per 100 people)": "Liechtenstein"
+  },
+  {
+    "2011": 83.4433166434182,
+    "Internet users (per 100 people)": "Germany"
+  },
+  {
+    "2011": 82.9877308713798,
+    "Internet users (per 100 people)": "Switzerland"
+  },
+  {
+    "2011": 82.6793444635074,
+    "Internet users (per 100 people)": "Canada"
+  },
+  {
+    "2011": 82,
+    "Internet users (per 100 people)": "Antigua and Barbuda"
+  },
+  {
+    "2011": 81.707337718108,
+    "Internet users (per 100 people)": "United Kingdom"
+  },
+  {
+    "2011": 81.4639615781755,
+    "Internet users (per 100 people)": "South Korea"
+  },
+  {
+    "2011": 81,
+    "Internet users (per 100 people)": "Andorra"
+  },
+  {
+    "2011": 80.7321728,
+    "Internet users (per 100 people)": "Faeroe Islands"
+  },
+  {
+    "2011": 79.7471949400166,
+    "Internet users (per 100 people)": "Austria"
+  },
+  {
+    "2011": 78.9480751173709,
+    "Internet users (per 100 people)": "Australia"
+  },
+  {
+    "2011": 78.7086520136867,
+    "Internet users (per 100 people)": "Japan"
+  },
+  {
+    "2011": 78.2362191947777,
+    "Internet users (per 100 people)": "United States"
+  },
+  {
+    "2011": 77.4843123779808,
+    "Internet users (per 100 people)": "Ireland"
+  },
+  {
+    "2011": 77,
+    "Internet users (per 100 people)": "Bahrain"
+  },
+  {
+    "2011": 76.769916674705,
+    "Internet users (per 100 people)": "France"
+  },
+  {
+    "2011": 76.5306570895522,
+    "Internet users (per 100 people)": "Estonia"
+  },
+  {
+    "2011": 76.2006148255814,
+    "Internet users (per 100 people)": "Belgium"
+  },
+  {
+    "2011": 75.0612448637074,
+    "Internet users (per 100 people)": "Singapore"
+  },
+  {
+    "2011": 75.0329390095594,
+    "Internet users (per 100 people)": "Hong Kong, China"
+  },
+  {
+    "2011": 74.8710678088235,
+    "Internet users (per 100 people)": "Slovak Republic"
+  },
+  {
+    "2011": 74.2,
+    "Internet users (per 100 people)": "Kuwait"
+  },
+  {
+    "2011": 72.8889967959416,
+    "Internet users (per 100 people)": "Czech Republic"
+  },
+  {
+    "2011": 72.4272155675676,
+    "Internet users (per 100 people)": "Latvia"
+  },
+  {
+    "2011": 71.7657,
+    "Internet users (per 100 people)": "Barbados"
+  },
+  {
+    "2011": 71.4039298245614,
+    "Internet users (per 100 people)": "Slovenia"
+  },
+  {
+    "2011": 70.5264460176991,
+    "Internet users (per 100 people)": "Croatia"
+  },
+  {
+    "2011": 70,
+    "Internet users (per 100 people)": "United Arab Emirates"
+  },
+  {
+    "2011": 69.46594499,
+    "Internet users (per 100 people)": "Cayman Islands"
+  },
+  {
+    "2011": 69.0308427207637,
+    "Internet users (per 100 people)": "Malta"
+  },
+  {
+    "2011": 68.1655974348739,
+    "Internet users (per 100 people)": "Israel"
+  },
+  {
+    "2011": 68,
+    "Internet users (per 100 people)": "Oman"
+  },
+  {
+    "2011": 67.9215075592084,
+    "Internet users (per 100 people)": "Spain"
+  },
+  {
+    "2011": 67.1719135341867,
+    "Internet users (per 100 people)": "Lithuania"
+  },
+  {
+    "2011": 65.0208240297258,
+    "Internet users (per 100 people)": "Poland"
+  },
+  {
+    "2011": 65,
+    "Internet users (per 100 people)": "Bahamas"
+  },
+  {
+    "2011": 64.6225856478218,
+    "Internet users (per 100 people)": "Greenland"
+  },
+  {
+    "2011": 61,
+    "Internet users (per 100 people)": "Malaysia"
+  },
+  {
+    "2011": 60,
+    "Internet users (per 100 people)": "Bosnia and Herzegovina"
+  },
+  {
+    "2011": 58.971100591716,
+    "Internet users (per 100 people)": "Hungary"
+  },
+  {
+    "2011": 58,
+    "Internet users (per 100 people)": "Macao, China"
+  },
+  {
+    "2011": 57.68,
+    "Internet users (per 100 people)": "Cyprus"
+  },
+  {
+    "2011": 57.06835805,
+    "Internet users (per 100 people)": "Aruba"
+  },
+  {
+    "2011": 56.8190326844607,
+    "Internet users (per 100 people)": "West Bank and Gaza"
+  },
+  {
+    "2011": 56.8174727530031,
+    "Internet users (per 100 people)": "Italy"
+  },
+  {
+    "2011": 56.7,
+    "Internet users (per 100 people)": "Macedonia, FYR"
+  },
+  {
+    "2011": 56,
+    "Internet users (per 100 people)": "Brunei"
+  },
+  {
+    "2011": 55.5737862085174,
+    "Internet users (per 100 people)": "Portugal"
+  },
+  {
+    "2011": 55.2,
+    "Internet users (per 100 people)": "Trinidad and Tobago"
+  },
+  {
+    "2011": 53.8938,
+    "Internet users (per 100 people)": "Chile"
+  },
+  {
+    "2011": 53.4033654458599,
+    "Internet users (per 100 people)": "Greece"
+  },
+  {
+    "2011": 52,
+    "Internet users (per 100 people)": "Lebanon"
+  },
+  {
+    "2011": 51.5741462538536,
+    "Internet users (per 100 people)": "Uruguay"
+  },
+  {
+    "2011": 51.3135,
+    "Internet users (per 100 people)": "Dominica"
+  },
+  {
+    "2011": 51.1678714859438,
+    "Internet users (per 100 people)": "New Caledonia"
+  },
+  {
+    "2011": 51,
+    "Internet users (per 100 people)": "Morocco"
+  },
+  {
+    "2011": 50.7962660513644,
+    "Internet users (per 100 people)": "Bulgaria"
+  },
+  {
+    "2011": 50.7527432373473,
+    "Internet users (per 100 people)": "Azerbaijan"
+  },
+  {
+    "2011": 49.6,
+    "Internet users (per 100 people)": "San Marino"
+  },
+  {
+    "2011": 49.3126343620094,
+    "Internet users (per 100 people)": "Russia"
+  },
+  {
+    "2011": 49,
+    "Internet users (per 100 people)": "Albania"
+  },
+  {
+    "2011": 49,
+    "Internet users (per 100 people)": "French Polynesia"
+  },
+  {
+    "2011": 48.5029090644214,
+    "Internet users (per 100 people)": "Puerto Rico"
+  },
+  {
+    "2011": 47.704,
+    "Internet users (per 100 people)": "Argentina"
+  },
+  {
+    "2011": 47.5,
+    "Internet users (per 100 people)": "Saudi Arabia"
+  },
+  {
+    "2011": 47.1929614929073,
+    "Internet users (per 100 people)": "Serbia"
+  },
+  {
+    "2011": 45,
+    "Internet users (per 100 people)": "Brazil"
+  },
+  {
+    "2011": 44.1156853623188,
+    "Internet users (per 100 people)": "Romania"
+  },
+  {
+    "2011": 44.0441800773852,
+    "Internet users (per 100 people)": "Kazakhstan"
+  },
+  {
+    "2011": 43.6051807381435,
+    "Internet users (per 100 people)": "Seychelles"
+  },
+  {
+    "2011": 43.01,
+    "Internet users (per 100 people)": "St. Vincent and the Grenadines"
+  },
+  {
+    "2011": 42.7,
+    "Internet users (per 100 people)": "Panama"
+  },
+  {
+    "2011": 42.12,
+    "Internet users (per 100 people)": "Costa Rica"
+  },
+  {
+    "2011": 42.1,
+    "Internet users (per 100 people)": "Turkey"
+  },
+  {
+    "2011": 42.0105,
+    "Internet users (per 100 people)": "St. Lucia"
+  },
+  {
+    "2011": 40.4382729701482,
+    "Internet users (per 100 people)": "Venezuela"
+  },
+  {
+    "2011": 40.4,
+    "Internet users (per 100 people)": "Colombia"
+  },
+  {
+    "2011": 40,
+    "Internet users (per 100 people)": "Montenegro"
+  },
+  {
+    "2011": 39.9613494774623,
+    "Internet users (per 100 people)": "Belarus"
+  },
+  {
+    "2011": 38.8078874159156,
+    "Internet users (per 100 people)": "Tunisia"
+  },
+  {
+    "2011": 38.3978870415808,
+    "Internet users (per 100 people)": "China"
+  },
+  {
+    "2011": 37.8490677156505,
+    "Internet users (per 100 people)": "Moldova"
+  },
+  {
+    "2011": 36.5,
+    "Internet users (per 100 people)": "Peru"
+  },
+  {
+    "2011": 36.15,
+    "Internet users (per 100 people)": "Mexico"
+  },
+  {
+    "2011": 35.7422582268241,
+    "Internet users (per 100 people)": "Jordan"
+  },
+  {
+    "2011": 35.62,
+    "Internet users (per 100 people)": "Egypt"
+  },
+  {
+    "2011": 35.5082538328573,
+    "Internet users (per 100 people)": "Mauritius"
+  },
+  {
+    "2011": 35.5,
+    "Internet users (per 100 people)": "Dominican Republic"
+  },
+  {
+    "2011": 35.450083102459,
+    "Internet users (per 100 people)": "Vietnam"
+  },
+  {
+    "2011": 35.2806933927775,
+    "Internet users (per 100 people)": "Georgia"
+  },
+  {
+    "2011": 34,
+    "Internet users (per 100 people)": "Maldives"
+  },
+  {
+    "2011": 32,
+    "Internet users (per 100 people)": "Cape Verde"
+  },
+  {
+    "2011": 32,
+    "Internet users (per 100 people)": "Guyana"
+  },
+  {
+    "2011": 32,
+    "Internet users (per 100 people)": "Suriname"
+  },
+  {
+    "2011": 31.9880040969992,
+    "Internet users (per 100 people)": "Jamaica"
+  },
+  {
+    "2011": 31.4,
+    "Internet users (per 100 people)": "Ecuador"
+  },
+  {
+    "2011": 30.2545942007741,
+    "Internet users (per 100 people)": "Ukraine"
+  },
+  {
+    "2011": 30,
+    "Internet users (per 100 people)": "Bolivia"
+  },
+  {
+    "2011": 30,
+    "Internet users (per 100 people)": "Tuvalu"
+  },
+  {
+    "2011": 29,
+    "Internet users (per 100 people)": "Philippines"
+  },
+  {
+    "2011": 28.5727940029719,
+    "Internet users (per 100 people)": "Uzbekistan"
+  },
+  {
+    "2011": 28.43,
+    "Internet users (per 100 people)": "Nigeria"
+  },
+  {
+    "2011": 28,
+    "Internet users (per 100 people)": "Fiji"
+  },
+  {
+    "2011": 28,
+    "Internet users (per 100 people)": "Kenya"
+  },
+  {
+    "2011": 25,
+    "Internet users (per 100 people)": "Tonga"
+  },
+  {
+    "2011": 23.9,
+    "Internet users (per 100 people)": "Paraguay"
+  },
+  {
+    "2011": 23.7,
+    "Internet users (per 100 people)": "Thailand"
+  },
+  {
+    "2011": 23.23,
+    "Internet users (per 100 people)": "Cuba"
+  },
+  {
+    "2011": 22.4413474179132,
+    "Internet users (per 100 people)": "Syria"
+  },
+  {
+    "2011": 21,
+    "Internet users (per 100 people)": "Bhutan"
+  },
+  {
+    "2011": 21,
+    "Internet users (per 100 people)": "Iran"
+  },
+  {
+    "2011": 20.9473704353098,
+    "Internet users (per 100 people)": "South Africa"
+  },
+  {
+    "2011": 20.4316581333298,
+    "Internet users (per 100 people)": "Swaziland"
+  },
+  {
+    "2011": 20.1612,
+    "Internet users (per 100 people)": "Sao Tome and Principe"
+  },
+  {
+    "2011": 20,
+    "Internet users (per 100 people)": "Mongolia"
+  },
+  {
+    "2011": 19.5844561467224,
+    "Internet users (per 100 people)": "Kyrgyz Republic"
+  },
+  {
+    "2011": 18,
+    "Internet users (per 100 people)": "Indonesia"
+  },
+  {
+    "2011": 17.68859598,
+    "Internet users (per 100 people)": "El Salvador"
+  },
+  {
+    "2011": 17.5,
+    "Internet users (per 100 people)": "Senegal"
+  },
+  {
+    "2011": 16.9981,
+    "Internet users (per 100 people)": "Libya"
+  },
+  {
+    "2011": 15.9,
+    "Internet users (per 100 people)": "Honduras"
+  },
+  {
+    "2011": 15.7,
+    "Internet users (per 100 people)": "Zimbabwe"
+  },
+  {
+    "2011": 15.1267866213043,
+    "Internet users (per 100 people)": "Sri Lanka"
+  },
+  {
+    "2011": 14.905,
+    "Internet users (per 100 people)": "Yemen"
+  },
+  {
+    "2011": 14.776,
+    "Internet users (per 100 people)": "Angola"
+  },
+  {
+    "2011": 14.11,
+    "Internet users (per 100 people)": "Ghana"
+  },
+  {
+    "2011": 14,
+    "Internet users (per 100 people)": "Algeria"
+  },
+  {
+    "2011": 13.03,
+    "Internet users (per 100 people)": "Tajikistan"
+  },
+  {
+    "2011": 13.01354333,
+    "Internet users (per 100 people)": "Uganda"
+  },
+  {
+    "2011": 12,
+    "Internet users (per 100 people)": "Namibia"
+  },
+  {
+    "2011": 12,
+    "Internet users (per 100 people)": "Tanzania"
+  },
+  {
+    "2011": 11.7274,
+    "Internet users (per 100 people)": "Guatemala"
+  },
+  {
+    "2011": 11.5,
+    "Internet users (per 100 people)": "Zambia"
+  },
+  {
+    "2011": 10.8703,
+    "Internet users (per 100 people)": "Gambia"
+  },
+  {
+    "2011": 10.6,
+    "Internet users (per 100 people)": "Nicaragua"
+  },
+  {
+    "2011": 10.07,
+    "Internet users (per 100 people)": "India"
+  },
+  {
+    "2011": 10,
+    "Internet users (per 100 people)": "Kiribati"
+  },
+  {
+    "2011": 9,
+    "Internet users (per 100 people)": "Lao"
+  },
+  {
+    "2011": 9,
+    "Internet users (per 100 people)": "Nepal"
+  },
+  {
+    "2011": 9,
+    "Internet users (per 100 people)": "Pakistan"
+  },
+  {
+    "2011": 8,
+    "Internet users (per 100 people)": "Gabon"
+  },
+  {
+    "2011": 7,
+    "Internet users (per 100 people)": "Botswana"
+  },
+  {
+    "2011": 7,
+    "Internet users (per 100 people)": "Djibouti"
+  },
+  {
+    "2011": 7,
+    "Internet users (per 100 people)": "Rwanda"
+  },
+  {
+    "2011": 6.2,
+    "Internet users (per 100 people)": "Eritrea"
+  },
+  {
+    "2011": 6,
+    "Internet users (per 100 people)": "Solomon Islands"
+  },
+  {
+    "2011": 5.6,
+    "Internet users (per 100 people)": "Congo, Rep."
+  },
+  {
+    "2011": 5.5,
+    "Internet users (per 100 people)": "Comoros"
+  },
+  {
+    "2011": 5,
+    "Internet users (per 100 people)": "Bangladesh"
+  },
+  {
+    "2011": 5,
+    "Internet users (per 100 people)": "Cameroon"
+  },
+  {
+    "2011": 5,
+    "Internet users (per 100 people)": "Turkmenistan"
+  },
+  {
+    "2011": 4.9549454873116,
+    "Internet users (per 100 people)": "Iraq"
+  },
+  {
+    "2011": 4.58066992077818,
+    "Internet users (per 100 people)": "Afghanistan"
+  },
+  {
+    "2011": 4.5,
+    "Internet users (per 100 people)": "Mauritania"
+  },
+  {
+    "2011": 4.3,
+    "Internet users (per 100 people)": "Mozambique"
+  },
+  {
+    "2011": 4.2248,
+    "Internet users (per 100 people)": "Lesotho"
+  },
+  {
+    "2011": 3.5,
+    "Internet users (per 100 people)": "Benin"
+  },
+  {
+    "2011": 3.5,
+    "Internet users (per 100 people)": "Togo"
+  },
+  {
+    "2011": 3.33,
+    "Internet users (per 100 people)": "Malawi"
+  },
+  {
+    "2011": 3.1,
+    "Internet users (per 100 people)": "Cambodia"
+  },
+  {
+    "2011": 3,
+    "Internet users (per 100 people)": "Burkina Faso"
+  },
+  {
+    "2011": 3,
+    "Internet users (per 100 people)": "Liberia"
+  },
+  {
+    "2011": 2.672,
+    "Internet users (per 100 people)": "Guinea-Bissau"
+  },
+  {
+    "2011": 2.2,
+    "Internet users (per 100 people)": "Central African Republic"
+  },
+  {
+    "2011": 2.2,
+    "Internet users (per 100 people)": "Cote d'Ivoire"
+  },
+  {
+    "2011": 2,
+    "Internet users (per 100 people)": "Mali"
+  },
+  {
+    "2011": 2,
+    "Internet users (per 100 people)": "Papua New Guinea"
+  },
+  {
+    "2011": 1.9,
+    "Internet users (per 100 people)": "Chad"
+  },
+  {
+    "2011": 1.9,
+    "Internet users (per 100 people)": "Madagascar"
+  },
+  {
+    "2011": 1.3,
+    "Internet users (per 100 people)": "Guinea"
+  },
+  {
+    "2011": 1.3,
+    "Internet users (per 100 people)": "Niger"
+  },
+  {
+    "2011": 1.25,
+    "Internet users (per 100 people)": "Somalia"
+  },
+  {
+    "2011": 1.2,
+    "Internet users (per 100 people)": "Congo, Dem. Rep."
+  },
+  {
+    "2011": 1.11,
+    "Internet users (per 100 people)": "Burundi"
+  },
+  {
+    "2011": 1.1,
+    "Internet users (per 100 people)": "Ethiopia"
+  },
+  {
+    "2011": 0.98,
+    "Internet users (per 100 people)": "Myanmar"
+  },
+  {
+    "2011": 0.88312633942239,
+    "Internet users (per 100 people)": "Timor-Leste"
+  }
+]
